cf7e94cd7a30231345e4ac06894c4759a6ca7981
Thiago Cordeiro <thiagoguetten@gmail.com>
2024-05-11 09:11:33 +0200

    Added additional portal build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,20 @@
-@file:Suppress("UnstableApiUsage")
-
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 object Publication {
     const val GROUP = "io.tcds.orm"
-    val buildVersion: String = System.getenv("VERSION") ?: "0.1.7"
+    val buildVersion: String = System.getenv("VERSION") ?: "dev"
+
+    object Sonatype {
+        val username: String? = System.getenv("OSS_USER")
+        val password: String? = System.getenv("OSS_PASSWORD")
+    }
+
+    object Gpg {
+        val signingKeyId: String? = System.getenv("GPG_KEY_ID")
+        val signingKey: String? = System.getenv("GPG_KEY")
+        val signingPassword: String? = System.getenv("GPG_KEY_PASSWORD")
+    }
 
     object Project {
         const val NAME = "Kotlin Simple ORM Migration Tools"
@@ -20,16 +29,14 @@ object Publication {
 
 plugins {
     `kotlin-dsl`
-
-    id("com.gradle.plugin-publish") version "1.2.1"
+    `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
 }
-
-group = Publication.GROUP
-version = Publication.buildVersion
-description = Publication.Project.DESCRIPTION
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
 
 dependencies {
@@ -42,21 +49,6 @@ dependencies {
     testImplementation("org.xerial:sqlite-jdbc:3.43.0.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     testImplementation("io.mockk:mockk:1.13.2")
-}
-
-gradlePlugin {
-    website.set(Publication.Project.REPOSITORY)
-    vcsUrl.set(Publication.Project.REPOSITORY)
-
-    plugins {
-        create("tcdsOrmMigrations") {
-            id = "${Publication.GROUP}.migrations"
-            implementationClass = "io.tcds.orm.migrations.plugin.OrmMigrationsPlugin"
-            displayName = Publication.Project.NAME
-            description = Publication.Project.DESCRIPTION
-            tags.set(listOf("orm", "migrations", "database"))
-        }
-    }
 }
 
 tasks.withType<KotlinCompile>().configureEach {
@@ -75,5 +67,78 @@ tasks.test {
         showCauses = true
         showStackTraces = true
         showStandardStreams = true
+    }
+}
+
+val sourcesJar by tasks.creating(Jar::class) { archiveClassifier.set("sources"); from(sourceSets.main.get().allSource) }
+val javadocJar by tasks.creating(Jar::class) { archiveClassifier.set("javadoc"); from(tasks.javadoc) }
+
+publishing {
+    repositories {
+        maven {
+            name = "SonaType"
+            group = Publication.GROUP
+            version = Publication.buildVersion
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+            credentials {
+                username = Publication.Sonatype.username
+                password = Publication.Sonatype.password
+            }
+        }
+    }
+
+    publications {
+        listOf("defaultMavenJava", "pluginMaven").forEach { publication ->
+            create<MavenPublication>(publication) {
+                artifact(sourcesJar)
+                artifact(javadocJar)
+
+                pom {
+                    name.set(Publication.Project.NAME)
+                    description.set(Publication.Project.DESCRIPTION)
+                    url.set(Publication.Project.REPOSITORY)
+                    packaging = "jar"
+
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("${Publication.Project.REPOSITORY}/blob/main/LICENSE")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set(Publication.Project.ORGANIZATION)
+                            name.set(Publication.Project.DEVELOPER)
+                        }
+                    }
+                    scm {
+                        connection.set(Publication.Project.SCM)
+                        url.set(Publication.Project.REPOSITORY)
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        Publication.Gpg.signingKeyId,
+        Publication.Gpg.signingKey,
+        Publication.Gpg.signingPassword,
+    )
+    sign(publishing.publications["pluginMaven"])
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(Publication.Sonatype.username)
+            password.set(Publication.Sonatype.password)
+        }
     }
 }
