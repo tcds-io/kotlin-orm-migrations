5a8b6e5877d4cc5542633249fb5f957baa9d9ca0
Thiago Cordeiro <thiagoguetten@gmail.com>
2024-05-15 09:11:50 +0200

    Improved existing migration check
    The check now loads all migrations at once and added a reverted column to indicate whenever the migration rolled back
    Added reverted column and

--- src/main/kotlin/io/tcds/orm/migrations/MigrationRunner.kt
+++ src/main/kotlin/io/tcds/orm/migrations/MigrationRunner.kt
@@ -2,6 +2,7 @@ package io.tcds.orm.migrations
 
 import io.tcds.orm.connection.Connection
 import io.tcds.orm.param.DateTimeParam
+import io.tcds.orm.param.IntegerParam
 import io.tcds.orm.param.StringParam
 import java.time.LocalDateTime
 import kotlin.reflect.KFunction
@@ -20,10 +21,14 @@ class MigrationRunner(private val connection: Connection) {
 
     fun runStatements(migrations: List<Statement>, log: (String) -> Unit) {
         createMigrationTableIfNeeded()
+        val migrated = loadPreviousMigrations()
 
         log("Running migrations...")
-        val executed = migrations.mapNotNull { migration -> migrate(migration) }
-        executed.forEach { file -> log(" - migrated ${file.name}") }
+
+        migrations
+            .filter { migration -> !migrated.contains(migration.name) }
+            .map { migration -> migrate(migration) }
+            .forEach { file -> log(" - migrated ${file.name}") }
 
         log("Done.")
     }
@@ -34,31 +39,29 @@ class MigrationRunner(private val connection: Connection) {
              CREATE TABLE IF NOT EXISTS `_migrations` (
                  `name`        VARCHAR(255),
                  `type`        VARCHAR(10),
+                 `reverted`    TINYINT NOT NULL DEFAULT 0,
                  `executed_at` DATETIME(6)  NOT NULL
              );
         """.trimIndent(),
         )
     }
 
-    private fun migrate(migration: Statement): Statement? {
-        val result = connection.read(
-            "SELECT count(*) as count FROM _migrations WHERE name = ?",
-            listOf(StringParam("name", migration.name)),
-        ).first()
+    private fun loadPreviousMigrations(): List<String> {
+        return connection.read("SELECT name FROM _migrations WHERE reverted = 0")
+            .map { result ->
+                result.value("name", String::class.java)!!
+            }.toList()
+    }
 
-        val total = result.value("count", Int::class.java)!!
-
-        if (total > 0) {
-            return null
-        }
-
+    private fun migrate(migration: Statement): Statement {
         connection.transaction {
             connection.write(migration.statement)
             connection.write(
-                "INSERT INTO _migrations VALUES (?, ?, ?)",
+                "INSERT INTO _migrations VALUES (?, ?, ?, ?)",
                 listOf(
                     StringParam("name", migration.name),
                     StringParam("type", migration.type.name),
+                    IntegerParam("reverted", 0),
                     DateTimeParam("executed_at", LocalDateTime.now()),
                 ),
             )
